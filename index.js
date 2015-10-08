55462aa5db8602657db4b2863b46c0e3511b6cc8
ayushajain <ayushajain@gmail.com>
2015-10-07 17:17:28 -0700

    setup

---
+++ index.js
@@ -0,0 +1,11 @@
+var canvas1 = document.getElementById('gameboy');
+canvas1.width = 1224;
+canvas1.height = 768;
+canvas1.style.zIndex = 8;
+canvas1.style.position = "absolute";
+canvas1.style.border = "1px solid";
+
+
+
+
+GameBoyCore(canvas1, "Asd");
