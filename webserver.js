cf825b5965da1721fd42a0ac0524dcba7ff1772a
ayushajain <ayushajain@gmail.com>
2015-10-08 20:58:52 -0700

    updates

---
+++ webserver.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app = express();
+
+app.use('/' , express.static(__dirname + '/'));
+
+app.listen(8080, function(){
+    console.log("listenting on http://localhost:8080");
+});
